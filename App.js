ca4a21e75b44a8603c8f805471b7ca24464c16df
SairaRaj Raj <saira.dhanyal97@gmail.com>
2020-11-02 21:52:03 +0500

    Second commit

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import {View, Text} from 'react-native';
 import {StackContainer} from './navigation/StackConatiner';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-//Hello
+
+
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'transparent'}}>
